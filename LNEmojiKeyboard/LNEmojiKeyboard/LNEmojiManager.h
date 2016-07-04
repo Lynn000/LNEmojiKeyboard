b0b29000d8f332c260d39baf3f2aad9b3e25535f
Lynn010 <Lynn000123@hotmail.com>
2016-07-04 23:24:41 +0800

    创建表情管理类
    1.创建表情管理类

---
+++ LNEmojiKeyboard/LNEmojiKeyboard/LNEmojiManager.h
@@ -0,0 +1,13 @@
+//
+//  LNEmojiManager.h
+//  LNEmojiKeyboard
+//
+//  Created by cong on 16/7/4.
+//  Copyright © 2016年 topcoders. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LNEmojiManager : NSObject
+
+@end
---
+++ LNEmojiKeyboard/LNEmojiKeyboard/LNEmojiManager.m
@@ -0,0 +1,15 @@
+//
+//  LNEmojiManager.m
+//  LNEmojiKeyboard
+//
+//  Created by cong on 16/7/4.
+//  Copyright © 2016年 topcoders. All rights reserved.
+//
+
+#import "LNEmojiManager.h"
+
+@implementation LNEmojiManager
+
+
+
+@end
